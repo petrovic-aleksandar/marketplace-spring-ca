d08f5ee89a133319870601cfb4171732c22740be
Aleksandar Petrović <aleksandar.petrovic.89@proton.me>
2026-01-01 12:35:51 +0100

    open some apis

--- src/main/java/me/aco/marketplace_spring_ca/infrastructure/config/SecurityConfig.java
+++ src/main/java/me/aco/marketplace_spring_ca/infrastructure/config/SecurityConfig.java
@@ -1,17 +1,14 @@
 package me.aco.marketplace_spring_ca.infrastructure.config;
 
-import me.aco.marketplace_spring_ca.infrastructure.security.JwtAuthenticationFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import me.aco.marketplace_spring_ca.infrastructure.security.JwtAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
@@ -26,20 +23,10 @@ public class SecurityConfig {
             .csrf(csrf -> csrf.disable())
             .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/api/Auth/**").permitAll()
+                .requestMatchers("/api/Auth/**", "/api/Item/Types", "/api/Item/byTypeId/*").permitAll()
                 .anyRequest().authenticated()
             )
             .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
-    @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
-        return config.getAuthenticationManager();
     }
 }
